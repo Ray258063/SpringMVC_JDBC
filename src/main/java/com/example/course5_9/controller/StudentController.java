b1a65ab480a137170002d3d14e38e59d8ad5b263
Ray <Chuang>
2024-02-23 00:43:25 +0800

    利用JDBC的方式連接資料庫，並建立三層式架構Controller Service Dao 實作從資料庫讀取學生資料

---
+++ src/main/java/com/example/course5_9/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.course5_9.controller;
+
+import com.example.course5_9.Student;
+import com.example.course5_9.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    @GetMapping("/students/{studentId}")
+    public Student select(@PathVariable Integer studentId){
+        return studentService.getById(studentId);
+    }
+
+}
