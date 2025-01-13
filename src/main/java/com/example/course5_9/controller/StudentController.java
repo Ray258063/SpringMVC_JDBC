28814aa284aa892ba9666bbba5461cab3dde8e6e
Ray <ray258063@gmail.com>
2025-01-13 15:12:58 +0800

    20250113修正springbootMVC template

--- src/main/java/com/example/course5_9/controller/StudentController.java
+++ src/main/java/com/example/course5_9/controller/StudentController.java
@@ -1,25 +1,22 @@
 package com.example.course5_9.controller;
 
 import com.example.course5_9.Student;
-import com.example.course5_9.bo.UserBo;
-import com.example.course5_9.config.KafkaConfig;
+
 import com.example.course5_9.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("kafka")
 public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @Autowired
-    private KafkaTemplate<String, UserBo> kafkaTemplate;
+
     @GetMapping("/students")
     public String test(){
         return "test sucess";
@@ -30,11 +27,5 @@ public class StudentController {
         return studentService.getById(studentId);
     }
 
-    @GetMapping("/students/{dpt}/{id}")
-    public String postUser(@PathVariable final String dpt, @PathVariable final String id){
-        UserBo userBo = new UserBo(dpt,id);
-        kafkaTemplate.send(KafkaConfig.JSON_TOPIC,userBo);
-        return "Published done";
-    }
 
 }
