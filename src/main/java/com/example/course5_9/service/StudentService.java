c052d86378ad5d4ef475347da861ab5d12e8451f
Ray <ray258063@gmail.com>
2025-03-31 23:58:38 +0800

    add comment3

--- src/main/java/com/example/course5_9/service/StudentService.java
+++ src/main/java/com/example/course5_9/service/StudentService.java
@@ -2,6 +2,7 @@ package com.example.course5_9.service;
 
 import com.example.course5_9.Student;
 
+//get student by id Service
 public interface StudentService {
     Student getById(Integer studentid);
 }
